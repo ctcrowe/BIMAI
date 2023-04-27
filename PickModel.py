194d377e22ad089577ab15de5f6a24e943eeac69
Charlie Crowe <charlietcrowe@gmail.com>
2023-04-27 09:18:33 -0700

    Update PickModel.py

--- PickModel.py
+++ PickModel.py
@@ -1,8 +1,16 @@
 import IndexNetwork as indexNetwork
 import DetailTypeNetwork as detailTypeNetwork
 import OLFNetwork as olfNetwork
+import SpaceFunctionNetwork as sfNetwork
 
 def pick(text):
+    if text == "Space Function":
+        txt_path = "Datasets/SpaceFunctionNetworkData.txt"
+        path = "Models/SpaceFunctionNetwork.pt"
+        model = sfNetwork.SpaceFunctionModel()
+        testMdl = sfNetwork.Test
+        dataset = sfNetowrk.SpaceFunctionDataset
+        return txt_path, path, model, testMdl, dataset
     if text == "Index":
         txt_path = "Datasets/IndexNetworkData.txt"
         path = "Models/IndexNetwork.pt"
